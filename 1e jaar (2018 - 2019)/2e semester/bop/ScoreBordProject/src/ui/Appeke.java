015086b9e0edfa5e54b62e92f6d0e90465b1a567
PieterHerremans <33780619+pietergmail@users.noreply.github.com>
2019-04-25 16:04:39 +0200

    finshing database boxen

---
+++ 1e jaar (2018 - 2019)/2e semester/bop/ScoreBordProject/src/ui/Appeke.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class Appeke {
+
+    public static void main(String[] args){
+        // maak een sccrebord-object voor 10 ploegen
+        // verhoog de score van de 10 ploegen een aantal keren met een aantal
+        // schrijf de scores van de ploegen uit op het scherm
+        // geef de top 3 ploegnummers terug met bijbehorende sores
+        // ploegen met de nummers uit de top 3 van vorige worden gediskwalificeerd, geef nu de top 3 terug
+    }
+}
