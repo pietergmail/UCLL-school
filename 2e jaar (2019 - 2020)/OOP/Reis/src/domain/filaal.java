bb2918d525a7c20aa879c6d354202a84a83b2d00
pietergmail <33780619+pietergmail@users.noreply.github.com>
2019-10-09 16:28:33 +0200

    Signed-off-by: pietergmail <33780619+pietergmail@users.noreply.github.com>

--- 2e jaar (2019 - 2020)/OOP/Reis/src/domain/filaal.java
+++ 2e jaar (2019 - 2020)/OOP/Reis/src/domain/filaal.java
@@ -1,4 +1,57 @@
 package domain;
 
+import domain.Reis;
+import domain.Bestemming;
+
+import java.util.ArrayList;
+
 public class filaal {
+    private ArrayList<Reis> reizen;
+    private String naam;
+
+    public filaal(ArrayList reizen, String naam) {
+        setReizen(reizen);
+        setNaam(naam);
+    }
+
+    public ArrayList getReizen() {
+        return reizen;
+    }
+
+    public void setReizen(ArrayList reizen) {
+        this.reizen = reizen;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    ///public ArrayList Getreizen(){
+        
+    ///}
+    
+    public int Getaantalreizen(){
+        return this.reizen.size();
+    }
+
+    public int Getlangstereis(){
+        int langst = 0;
+        for (Reis reis: this.reizen) {
+            if(reis.getDagen() > langst){
+                langst = reis.getDagen();
+            }
+        }
+        return langst;
+    }
+
+    public String populairstebestemming(){
+        Reis populairste;
+        for (Reis reis: this.reizen){
+
+        }
+    }
 }
